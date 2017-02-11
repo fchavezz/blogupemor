d5ea411a3bff3013aad37c838667bb324fdad987
fchavez <francisco.fwss@gmail.com>
2017-02-11 11:34:14 -0600

    Cambio de redireccionamiento a /noticias

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@
 Route::get('/', 'PruebasController@index');
 Route::resource('prueba', 'PruebasController');
 Route::get('noticias', 'PruebasController@note');
-
 Route::put('prueba/{id}', 'PruebasController@update');
 //Route::put('prueba/{id}', 'PruebasController@update');
 Route::get('/noticiaimagen/{filename}',[
